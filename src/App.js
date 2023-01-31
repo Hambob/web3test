c83895a58d9782f19769c056b2263f9a9db797be
Hamzah Abokreen <hamzah.abokreen@gmail.com>
2023-01-31 17:08:25 +0200

    [feat]: SwitchNetworks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import './App.css';
-import { useAccount, useConnect, useDisconnect, useEnsName, useBalance } from 'wagmi'
-import { InjectedConnector } from 'wagmi/connectors/injected'
+import "./App.css";
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useBalance,
+  useSwitchNetwork,
+} from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
 
 function App() {
-  const { address, isConnected } = useAccount()
-  const {disconnect} = useDisconnect()
+  const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
   const { connect } = useConnect({
     connector: new InjectedConnector(),
-  })
-  const { data: bl, isError, isLoading } = useBalance({
+  });
+  const {
+    data: bl,
+    isError,
+    isLoading,
+  } = useBalance({
     address,
-  })
-  if (isLoading) return <div>Fetching balance…</div>
-  if (isError) return <div>Error fetching balance</div>
+  });
+
+  const { chain, chains, switchNetwork, pendingChainId, error } =
+    useSwitchNetwork();
+
+  if (isLoading) return <div>Fetching balance…</div>;
+  if (isError) return <div>Error fetching balance</div>;
+
   return (
     <div className="App">
-        <div className="card">
-          {isConnected && (
-            <div>Address: <p>{address}</p> ETH Balance: <p>{bl?.formatted} {bl?.symbol}</p> <hr /></div>
-          )}
-          {!isConnected && <button className='btn connect' onClick={() => connect()}>Connect</button>}
-          {isConnected && <button className='btn close' onClick={() => disconnect()}>Disconnect</button>}
-
-        </div>
+      <div className="card">
+        {isConnected ? (
+          <div>
+            Address: <p>{address}</p> ETH Balance:{" "}
+            <p>
+              {bl?.formatted} {bl?.symbol}
+            </p>
+            {chain && <p> Current Network: {chain?.name} </p>}
+            <select
+              onChange={(e) => switchNetwork(parseInt(e.target.value))}
+              value={chain?.id}
+            >
+              {chains.map((c) => (
+                <option key={c.id} id={c.id} value={c.id}>
+                  {c.name}
+                </option>
+              ))}
+            </select>
+            <hr />
+          </div>
+        ) : (
+          <button className="btn connect" onClick={() => connect()}>
+            Connect
+          </button>
+        )}
+        {isConnected && (
+          <button className="btn close" onClick={() => disconnect()}>
+            Disconnect
+          </button>
+        )}
+      </div>
     </div>
   );
 }
